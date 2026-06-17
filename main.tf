5f4f4cfb4a9d51980d9881955eeee2295e29d581
thorvald <thor.gudj@gmail.com>
2026-06-17 20:06:30 +0200

    Removed duplicate code

--- main.tf
+++ main.tf
@@ -1,12 +1,4 @@
 terraform {
-  required_version = ">= 1.5"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-  }
-
   # Will be filled later
   backend "azurerm" {}
 }
