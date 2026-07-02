4d99ccebaeb029db5c89209b838ddf9763adf441
thorvald <thor.gudj@gmail.com>
2026-07-02 12:46:34 +0200

    Add permanent deletion of keyvault after destroy

--- providers.tf
+++ providers.tf
@@ -3,6 +3,13 @@ provider "azurerm" {
     log_analytics_workspace {
       permanently_delete_on_destroy = true
     }
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = false
+
+      purge_soft_deleted_secrets_on_destroy = true
+      recover_soft_deleted_secrets          = false
+    }
   }
   
 }
