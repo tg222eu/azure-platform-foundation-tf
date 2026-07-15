745e511e09967a7690520ab37769af9e5b840c82
thorvald <thor.gudj@gmail.com>
2026-07-15 19:53:34 +0200

    Add Actiongroup and budget in alert.tf with var

---
+++ alert.tf
@@ -0,0 +1,74 @@
+# =============================================================================
+# Action Group
+# =============================================================================
+
+resource "azurerm_monitor_action_group" "platform_alerts" {
+    name = "alerts-platform-dev"
+    resource_group_name = azurerm_resource_group.platform.name
+    short_name = "plat-alerts"
+
+    email_receiver {
+      name = "platform-email"
+      email_address = var.alert_email
+      use_common_alert_schema = true
+    }
+    
+    tags = local.common_tags
+}
+
+# =============================================================================
+# Budget
+# =============================================================================
+
+resource azurerm_consumption_budget_resource_group "platform_budget" {
+    name                = var.consumption_budget_name
+    resource_group_id   = azurerm_resource_group.platform
+    time_grain          = "Monthly"
+    amount              = 100 # SEK currency
+
+    time_period {
+      start_date = formatdate("YYYY-MM-01'T'00:00:00Z", timestamp())
+    }
+
+    notification {
+        enabled = true
+        threshold = 10
+        operator = "GreaterThan"
+        contact_emails = [var.alert_email]
+        contact_groups = [azurerm_monitor_action_group.platform_alerts.id]
+    }
+
+    notification {
+        enabled = true
+        threshold = 20
+        operator = "GreaterThan"
+        contact_emails = [var.alert_email]
+        contact_groups = [azurerm_monitor_action_group.platform_alerts.id]
+    }
+
+    notification {
+        enabled = true
+        threshold = 40
+        operator = "GreaterThan"
+        contact_emails = [var.alert_email]
+        contact_groups = [azurerm_monitor_action_group.platform_alerts.id]
+    }
+    
+    notification {
+        enabled = true
+        threshold = 70
+        operator = "GreaterThan"
+        contact_emails = [var.alert_email]
+        contact_groups = [azurerm_monitor_action_group.platform_alerts.id]
+    }
+
+    notification {
+        enabled = true
+        threshold = 100
+        operator = "GreaterThan"
+        contact_emails = [var.alert_email]
+        contact_groups = [azurerm_monitor_action_group.platform_alerts.id]
+    }
+
+depends_on = [azurerm_monitor_action_group.platform_alerts]
+}
--- variables.tf
+++ variables.tf
@@ -11,53 +11,65 @@ variable "location" {
     default     = "Sweden Central"
 }
 
-variable "platform_resource_group_name"{
+variable "platform_resource_group_name" {
     description = "Resource group"
     type        = string
     default     = "rg-platform-dev"
 }
 
-variable "virtual_network_name"{
+variable "virtual_network_name" {
     description = "App subnet"
     type        = string
     default     = "vnet-platform-dev"
+}
+
+variable "consumption_budget_name" {
+    description = "Budget consumption name"
+    type = string
+    default = "budget-platform-dev"
+}
+
+variable "alert_email" {
+    description = "Email which alerts are sent to"
+    type = string
+    default = "example@example.com"
 }
 
 # =============================================================================
 # SUBNETS
 # =============================================================================
 
-variable "app_subnet_name"{
+variable "app_subnet_name" {
     description = "App subnet name"
     type        = string
     default     = "snet-app-platform-dev"
 }
 
-variable "data_subnet_name"{
+variable "data_subnet_name" {
     description = "Data subnet name"
     type        = string
     default     = "snet-data-platform-dev"
 }
 
-variable "management_subnet_name"{
+variable "management_subnet_name" {
     description = "Management subnet name"
     type        = string
     default     = "snet-mgmt-platform-dev"
 }
 
-variable "app_subnet_address_prefix"{
+variable "app_subnet_address_prefix" {
     description = "App subnet address prefix"
     type        = string
     default     = "10.0.1.0/24"
 }
 
-variable "data_subnet_address_prefix"{
+variable "data_subnet_address_prefix" {
     description = "Data subnet address prefix"
     type        = string
     default     = "10.0.2.0/24"
 }
 
-variable "management_subnet_address_prefix"{
+variable "management_subnet_address_prefix" {
     description = "Management address prefix"
     type        = string
     default     = "10.0.3.0/24"
@@ -67,19 +79,19 @@ variable "management_subnet_address_prefix"{
 # NSG
 # =============================================================================
 
-variable "app_nsg_name"{
+variable "app_nsg_name" {
     description = "App NSG"
     type        = string
     default     = "nsg-app-platform-dev"
 }
 
-variable "data_nsg_name"{
+variable "data_nsg_name" {
     description = "Data NSG"
     type        = string
     default     = "nsg-data-platform-dev"
 }
 
-variable "mgmt_nsg_name"{
+variable "mgmt_nsg_name" {
     description = "Management NSG"
     type        = string
     default     = "nsg-mgmt-platform-dev"
