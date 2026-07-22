9ef2240729b5c098236ab2c3e85164889e38f3f6
thorvald <thor.gudj@gmail.com>
2026-07-22 22:47:11 +0200

    Version update

--- oidc.tf
+++ oidc.tf
@@ -1,3 +1,7 @@
 # =============================================================================
 # GitHub OpenID Connect
 # =============================================================================
+
+resource "azuread_application" "main" {
+  display_name = "github-oidc-platformfoundation"
+}
